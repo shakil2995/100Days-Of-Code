d9defad03bb1785423ff44bd2b7e310b436dd01a
shakil2995 <shakil2995@gmail.com>
2021-02-04 15:34:20 +0600

    d2

--- Day2.py
+++ Day2.py
@@ -1,4 +1,13 @@
-number=0
-print("input your number : ")
-number=input()
-print(number)
+#                                Age calculator
+#age=90-int(input("Enter your age : "))
+#print(f"You have {age*365} days , {age *52} weeks , and {age*12} months left")
+
+#                                Tip Calculator
+# print("Welcome to the tip calculator.")
+# bill=float(input("What is the total bill ? :"))
+# percent=int(input("What percentage tip would you like to give ? 10 ,12 or 15 ? : "))
+# noOfPeople=int(input("how many people to split the bill ? : "))
+# bill="{:.2f}".format((bill+bill*percent/100)/noOfPeople)
+# print(f"Each person should pay : {bill}")
+
+#                                
