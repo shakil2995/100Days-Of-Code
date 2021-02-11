70b282c2d3959183ae772c71d4450ee952ac6a03
shakil2995 <shakil2995@gmail.com>
2021-02-12 00:52:26 +0600

    Update day7 hangman.py

--- day7 hangman.py
+++ day7 hangman.py
@@ -5,15 +5,14 @@ display=[]
 index=0
 #chosen_word=random.choice(word_list)
 chosen_word="baboon"
-guess=input("Enter a letter : ").lower()
 for letter in chosen_word:
     display.append("_")
 print (chosen_word)
-for i in range(0,100):
-    if "_" not in display:
-        break
+print(display)
+guess=input("Enter a letter : ")
+while "_" not in display:
     for letter in chosen_word:
         if letter.lower()==guess.lower():
             display[index]=f"{guess}"
-    index=index+1   
+        index=index+1   
     print(display)
