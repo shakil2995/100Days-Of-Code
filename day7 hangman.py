31e4372485c0395aeb14f74e720c31e71f1dfb2e
shakil2995 <shakil2995@gmail.com>
2021-02-12 01:28:32 +0600

    Update day7 hangman.py

--- day7 hangman.py
+++ day7 hangman.py
@@ -1,18 +1,82 @@
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+lives=6
 word_list=["tiger","baboon","camel"]
 guess=""
 display=[]
 index=0
-#chosen_word=random.choice(word_list)
-chosen_word="baboon"
-for letter in chosen_word:
+chosen_word=random.choice(word_list)
+print(chosen_word)
+for position in range(len(chosen_word)):
     display.append("_")
-print (chosen_word)
-print(display)
-guess=input("Enter a letter : ")
-while "_" not in display:
-    for letter in chosen_word:
+end_of_game=False
+while not end_of_game:
+    guess=input("Guess a letter : ")
+    for position in range(len(chosen_word)):
+        letter=chosen_word[position]
         if letter.lower()==guess.lower():
-            display[index]=f"{guess}"
-        index=index+1   
+            display[position]=letter
+            if "_" not in display:
+                end_of_game=True
+                print("You Win")
+        elif letter.lower()!=guess.lower():
+            print(stages[lives])
+            lives=lives-1          
     print(display)
+    
+
