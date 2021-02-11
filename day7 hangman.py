021f1fad0adf649751700dce4a94b839b34e37ec
shakil2995 <shakil2995@gmail.com>
2021-02-12 00:36:24 +0600

    Update day7 hangman.py

--- day7 hangman.py
+++ day7 hangman.py
@@ -1,12 +1,19 @@
 import random
 word_list=["tiger","baboon","camel"]
 guess=""
+display=[]
+index=0
 #chosen_word=random.choice(word_list)
-chosen_word="Tiogoer"
+chosen_word="baboon"
 guess=input("Enter a letter : ").lower()
-print(guess)
-for letters in word_list:
-    if letters.lower()==guess.lower():
-        print("True")
-    else:
-        print ("false")
+for letter in chosen_word:
+    display.append("_")
+print (chosen_word)
+for i in range(0,100):
+    if "_" not in display:
+        break
+    for letter in chosen_word:
+        if letter.lower()==guess.lower():
+            display[index]=f"{guess}"
+    index=index+1   
+    print(display)
