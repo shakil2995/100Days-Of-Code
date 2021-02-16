6499b7229963df1ed1fb806cfed638347953b4db
shakil2995 <shakil2995@gmail.com>
2021-02-17 02:08:05 +0600

    Update Day8_Caesar_Cipher.py

--- Day8_Caesar_Cipher.py
+++ Day8_Caesar_Cipher.py
@@ -1,14 +1,38 @@
 import math
-def calculateCan(height,width,coverage):
-    NoC=(height*width)/coverage
-    return math.ceil(NoC)
+import os
+import time
+#                                         paint calculator
+# def calculateCan(height,width,coverage):
+#     NoC=(height*width)/coverage
+#     return math.ceil(NoC)
                                 
-height=int(input("Enter Height of wall : "))
-width=int(input("Width of wall : "))
-cover=int(input("Enter coverge : "))
-print(f"You need {calculateCan(height=height,width=width,coverage=cover)} cans of Paint")
+# height=int(input("Enter Height of wall : "))
+# width=int(input("Width of wall : "))
+# cover=int(input("Enter coverge : "))
+# print(f"You need {calculateCan(height=height,width=width,coverage=cover)} cans of Paint")
 
+#                       Prime number checker
 
+def CheckIfPrime(number):
+    if number==1:
+        return "Prime"
+    for i in range(2,number):
+        if number%i==0:
+            return "not a Prime"
+    return "a Prime"
+
+flag=True
+while flag:
+    os.system('cls')
+    number=int(input("Input number to check : "))
+    print(f"{number} is {CheckIfPrime(number)} number")
+    choice=input("\nRun again ? : yes / no : ")
+    if choice.lower()=='yes':
+        flag=True
+    else:
+        print("Program will now exit.")
+        time.sleep(1)
+        flag=False
 
 
 
