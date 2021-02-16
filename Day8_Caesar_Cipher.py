cc7da92a49d0a9d8f525006f4ab01b9465e6e5f5
shakil2995 <shakil2995@gmail.com>
2021-02-17 02:10:59 +0600

    Update Day8_Caesar_Cipher.py

--- Day8_Caesar_Cipher.py
+++ Day8_Caesar_Cipher.py
@@ -13,26 +13,28 @@ import time
 
 #                       Prime number checker
 
-def CheckIfPrime(number):
-    if number==1:
-        return "Prime"
-    for i in range(2,number):
-        if number%i==0:
-            return "not a Prime"
-    return "a Prime"
+# def CheckIfPrime(number):
+#     if number==1:
+#         return "Prime"
+#     for i in range(2,number):
+#         if number%i==0:
+#             return "not a Prime"
+#     return "a Prime"
 
-flag=True
-while flag:
-    os.system('cls')
-    number=int(input("Input number to check : "))
-    print(f"{number} is {CheckIfPrime(number)} number")
-    choice=input("\nRun again ? : yes / no : ")
-    if choice.lower()=='yes':
-        flag=True
-    else:
-        print("Program will now exit.")
-        time.sleep(1)
-        flag=False
+# flag=True
+# while flag:
+#     os.system('cls')
+#     number=int(input("Input number to check : "))
+#     print(f"{number} is {CheckIfPrime(number)} number")
+#     choice=input("\nRun again ? : yes / no : ")
+#     if choice.lower()=='yes':
+#         flag=True
+#     else:
+#         print("Program will now exit.")
+#         time.sleep(1)
+#         flag=False
+
+#                          Caesar Cipher
 
 
 
