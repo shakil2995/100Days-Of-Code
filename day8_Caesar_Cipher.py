d9e8e4d22dcb8b49d1b7c2ad507c7144fbb1bcfc
shakil2995 <shakil2995@gmail.com>
2021-02-17 22:46:28 +0600

    Update day8_Caesar_Cipher.py

--- day8_Caesar_Cipher.py
+++ day8_Caesar_Cipher.py
@@ -44,8 +44,8 @@ def encrypt(text,shift):
     enMessage=''
     for letter in text:
         LetterIndex=alphabet.index(letter)
-        if LetterIndex>=20:
-            LetterIndex=0
+        if LetterIndex>=26-shift:
+            LetterIndex=-shift
         enList.append(alphabet[LetterIndex+shift])
     for i in enList:
         enMessage+=i
@@ -57,33 +57,16 @@ def decrypt(text,shift):
     for letter in text:
         LetterIndex=alphabet.index(letter)
         print(LetterIndex)
-        if LetterIndex<=5:
-            LetterIndex=26
+        print(alphabet[LetterIndex])
+        if LetterIndex+shift<0:
+            LetterIndex+=shift
+            print(LetterIndex)
         enList.append(alphabet[LetterIndex-shift])
     for i in enList:
         enMessage+=i
     print(enMessage)
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-# text = input("Type your message:\n").lower()
-# shift = int(input("Type the shift number:\n"))
 
-# #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-#     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-#     #e.g. 
-#     #plain_text = "hello"
-#     #shift = 5
-#     #cipher_text = "mjqqt"
-#     #print output: "The encoded text is mjqqt"
-
-#     ##HINT: How do you get the index of an item in a list:
-#     #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-#     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-# #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-# encrypt(text,shift)
-encrypt("civilization",5)
-decrypt('hnfnqnffynts',5)
+encrypt("civilization",1)
+decrypt('djwjmjabujpo',1)
 
 
