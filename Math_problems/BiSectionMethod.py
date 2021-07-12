1708648e70fbc24779effc21e72b27b4456cbcaf
Shakil <shakil2995@gmail.com>
2021-07-12 18:50:42 +0600

    Update BiSectionMethod.py

--- Math_problems/BiSectionMethod.py
+++ Math_problems/BiSectionMethod.py
@@ -1,25 +1,24 @@
+import os
 
-# function x^2+x-1;
-ErrorRate = .001
+ErrorRate = 0
 def func(x):
-    return x*x + x -1
+    return x*x*x - 4*x -9
 
-#  Prints root of func(x) with ErrorRate
 def bisection(a,b):
     if (func(a) * func(b) >= 0):
-            print("You have not assumed right a and b\n")
+            print("Root not found for a and b\n")
             return
     counts=0
     c = a
     while ((b-a) >= ErrorRate):
         counts=counts+1
-            # Find middle point
+
         c = (a+b)/2
-        # Check if middle point is root
+ 
         if (func(c) == 0.0):
             break
 
-        # Decide the side to repeat the steps
+
         elif (func(c)*func(a) < 0):
             b = c
         else:
@@ -30,10 +29,11 @@ def bisection(a,b):
         print("Total Number of iteration=",counts)
         print("The value of root is : ",c)
 
-# Driver program to test above function
-    # Initial values assumed
 a=b=0
-print("x^2+x-1")
-a=int(input("initial value for lower limit="))
-b=int(input("initial value for upper limit="))
+print("Solving function : x3-4x-9 using Bisection Method \n" )
+a=float(input("Initial value for lower limit="))
+b=float(input("Initial value for upper limit="))
+ErrorRate=float(input("Give error rate : "))
 bisection(a, b)
+os.system("pause")
+#x3-4x-9
