90e659da7e30834cd52f92f4eb2c2403483b76d9
Shakil <shakil2995@gmail.com>
2021-07-12 13:22:45 +0600

    Update Scratch.py

--- Scratch.py
+++ Scratch.py
@@ -1,6 +1,8 @@
-chosen_word='Hello'
-flag=True
-for position in range(len(chosen_word)):
-    if flag==True and position==len(chosen_word)-1:
-        #print(len(chosen_word)-1)
-        print(position)
+# chosen_word='Hello'
+# flag=True
+# for position in range(len(chosen_word)):
+#     if flag==True and position==len(chosen_word)-1:
+#         #print(len(chosen_word)-1)
+#         print(position)
+import cv2 # import the opencv library.
+print (cv2. __version__) # this will print the version of your opencv3.
